11dfc60358721bb638bacec2a79c030e7c02483a
Molly <dolanmollye@gmail.com>
2021-01-13 21:35:05 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(nameArr, event) {
+    let thankYouMessage = [];
+    for (let i = 0; i < nameArr.length; i++) {
+    thankYouMessage.push( `Thank you, ${nameArr[i]}, for the wonderful ${event} gift!`)
+    };
+    return thankYouMessage
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
+
